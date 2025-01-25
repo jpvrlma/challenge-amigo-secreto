3b904db9b2b0a1c25074c9528501f3ab425fbdd4
Joao Pedro <joao@users.github.com>
2025-01-24 21:24:29 -0300

    Adicionar função de limpeza de campos e adição de elementos na lista

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    if(amigo == ""){
+        alert('Campo vazio, por favor insira um nome!');
+    } else {
+        amigos.push(amigo);
+        limparCampo();
+    }
+    console.log(amigos);
+}
+
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
