87e76881139d05485a3c0bb8182d4a825d955d2f
Joao Pedro <joao@users.github.com>
2025-01-25 14:22:26 -0300

    Adicionar função de exibir elementos do array no site

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 let amigos = [];
 
 function adicionarAmigo() {
-    let amigo = document.querySelector('input').value;
-    if(amigo == ""){
-        alert('Campo vazio, por favor insira um nome!');
-    } else {
-        amigos.push(amigo);
-        limparCampo();
-    }
-    console.log(amigos);
+  let amigo = document.querySelector("input").value;
+  if (amigo == "") {
+    alert("Campo vazio, por favor insira um nome!");
+  } else {
+    amigos.push(amigo);
+    limparCampo();
+  }
+  atualizarListaDeAmigos();
 }
 
 function limparCampo() {
-    campo = document.querySelector('input');
-    campo.value = '';
+  campo = document.querySelector("input");
+  campo.value = "";
+}
+
+function atualizarListaDeAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  for (i = 0; i < amigos.length; i++) {
+    let lista = document.createElement("li");
+    lista.textContent = amigos[i];
+    listaAmigos.appendChild(lista);
+  }
 }
